dd16c2655853e8325cbd6d5883c6b90e6028db83
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-03-13 02:17:42 +0000

    Create KNearestNeighbor.py

---
+++ mylearn/KNearestNeighbor.py
@@ -0,0 +1,20 @@
+from collections import Counter
+import numpy as np
+
+
+class KNearestNeighbor:
+
+    def __init__(self, k=5):
+        self.k = k
+
+    def fit(self, x, y):
+        self._x_train = x
+        self._y_train = y
+        return self
+
+    def predict(self, x):
+        distance = ((self._x_train[:, np.newaxis, :] - x[np.newaxis, :, :]) ** 2).sum(axis=-1)
+        top_k_index = distance.argsort(axis=0)[:, :self.k]
+        top_k_class = self._y_train[top_k_index]
+        most_common_class = np.apply_along_axis(lambda x: Counter(x).most_common()[0][0], axis=1, arr=top_k_class)
+        return most_common_class
