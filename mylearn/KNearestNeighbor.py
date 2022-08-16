a610a2ceaabc29093e8b328fc94731e42e28bbf6
Mao Li <63109264+maoli67660@users.noreply.github.com>
2022-08-16 16:57:26 +0100

    Update KNearestNeighbor.py
    Rewrite Knn with pure dataset.
    Add a test case on the iris dataset.

--- mylearn/KNearestNeighbor.py
+++ mylearn/KNearestNeighbor.py
@@ -1,20 +1,67 @@
 from collections import Counter
-import numpy as np
 
-
-class KNearestNeighbor:
-
-    def __init__(self, k=5):
+def eculid_dis(x,y):
+        distance = 0
+        for i in range(len(x)):
+            distance += (x[i]-y[i])**2
+        return distance**0.5
+    
+class Knn:
+    def __init__(self, distance_func, k=5):
+        self.train_x = None
         self.k = k
+        self.distance_func = distance_func
+    
+    def fit(self, train_x, train_y):
+        self.train_x = train_x
+        self.train_y = train_y
+    
+    def predict(self, test_x):
+        predictions = []
+        for test_vec in test_x:
+            distance_ls = []
+            for index, train_vec in enumerate(self.train_x):
+                dist = self.distance_func(test_vec, train_vec)
+                distance_ls.append([dist,index])
+            nn_k_index = [i[1] for i in sorted(distance_ls)[:self.k]]
+            
+            top_k_labels = []
+            for index in nn_k_index:
+                top_k_labels.append(self.train_y[index])
+            
+            predict_label = Counter(top_k_labels).most_common(1)[0][0]
+            predictions.append(predict_label)
+        return predictions
+    
+    ##########################################################################################################
+    
+from sklearn.datasets import load_iris
+from sklearn.metrics import classification_report
 
-    def fit(self, x, y):
-        self._x_train = x
-        self._y_train = y
-        return self
+iris = load_iris()
 
-    def predict(self, x):
-        distance = ((self._x_train[:, np.newaxis, :] - x[np.newaxis, :, :]) ** 2).sum(axis=-1)
-        top_k_index = distance.argsort(axis=0)[:, :self.k]
-        top_k_class = self._y_train[top_k_index]
-        most_common_class = np.apply_along_axis(lambda x: Counter(x).most_common()[0][0], axis=1, arr=top_k_class)
-        return most_common_class
+test_n = 30
+X_train = iris['data'][:-test_n]
+y_train = iris['target'][:-test_n]
+
+X_test = iris['data'][test_n:]
+y_test = iris['target'][test_n:]
+
+
+clf = Knn(eculid_dis)
+clf.fit(X_train, y_train)
+print(classification_report(y_test, clf.predict(X_test)))
+
+
+'''
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00        20
+           1       0.86      1.00      0.93        50
+           2       1.00      0.84      0.91        50
+
+    accuracy                           0.93       120
+   macro avg       0.95      0.95      0.95       120
+weighted avg       0.94      0.93      0.93       120
+'''
+
